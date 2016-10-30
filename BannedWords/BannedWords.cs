0b1988dbbda4ca93c019b9565d903378da92f790
Ryozuki <clud6565@gmail.com>
2016-10-30 11:39:53 +0100

    Remove useless usings

--- BannedWords/BannedWords.cs
+++ BannedWords/BannedWords.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
-using TShockAPI.Hooks;
 
 namespace BannedWords
 {
